9613e42ea2ec5300b9845b4692febd41e3e5d1f3
Dileepa Lakmina <dileepa2018al1997@gmail.com>
2021-05-11 01:12:26 +0530

    finished update

--- SQL(CRUD)/update.php
+++ SQL(CRUD)/update.php
@@ -1,4 +1,40 @@
 <?php
+include "./include/connection.php";
+$idGet = "";
+
+if (isset($_GET["ID"])) {
+  $idGet = $_GET["ID"];
+  $querry = "SELECT * FROM signup WHERE id = {$_GET["ID"]}";
+  $result = mysqli_query($conn, $querry);
+
+  if ($result) {
+    if ($user = mysqli_fetch_assoc($result)) {
+      $name = $user["name"];
+      $email = $user["email"];
+      $password = $user["password"];
+    }
+  }
+  if (isset($_POST["submit"])) {
+    if (!empty($_POST["name"]) || !empty($_POST["email"]) || !empty($_POST["password"])) {
+      $name = $_POST["name"];
+      $email = $_POST["email"];
+      $password = $_POST["password"];
+
+      $query  = "UPDATE signup SET name=' $name',email='$email', password = '$password'  LIMIT 1 ";
+      $result = mysqli_query($conn, $query);
+      if ($result) {
+        echo "update success";
+        header("Location:./insert.php?success_update");
+      } else {
+        echo "failed.";
+        header("Location:./insert.php?failed_update");
+      }
+    }
+  }
+}
+
+
+
 
 ?>
 
@@ -23,13 +59,13 @@
         <legend>
           <h3>Form</h3>
         </legend>
-        <form action="insert.php" method="post">
+        <form action="update.php?ID=<?php $_GET["ID"] ?>" method="post">
           <label for="name">Name :</label>
-          <input type="text" placeholder="Name" name="name" /><span style="color:red"><?php echo $error; ?></span>
+          <input type="text" placeholder="Name" name="name" value="<?php echo "{$name}"; ?>" /><span style="color:red"><?php  ?></span>
           <label for=" email">Email :</label>
-          <input type="email" placeholder="Email" name="email" /><span style="color:red"><?php echo $error; ?></span>
+          <input type="email" placeholder="Email" name="email" value="<?php echo "{$email}"; ?>" /><span style="color:red"><?php  ?></span>
           <label for=" password">Password :</label>
-          <input type="password" placeholder="Password" name="password" /><span style="color:red"><?php echo $error; ?></span>
+          <input type="password" placeholder="Password" name="password" value="<?php echo "{$password}"; ?>" /><span style="color:red"><?php ?></span>
           <button type="submit" name="submit">Submit</button>
         </form>
       </fieldset>
